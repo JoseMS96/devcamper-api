315e9c3427e7b4a23bea98b7d45cf1fdf1255eb2
Jose <melo.jeduardo@gmail.com>
2024-08-16 14:31:51 -0300

    Adicionado paginação, limite por página, filtros, ordenação e seleção de campos a serem trazidos

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,18 +1,84 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
+const geocoder = require('../utils/geocoder');
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc    Traz dados de todos bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  // Copiando req.query
+  const reqQuery = { ...req.query };
 
-  res
-    .status(200)
-    .json({ success: true, count: bootcamps.length, data: bootcamps });
+  // Campos a excluir da query
+  const removeFields = ['select', 'sort', 'page', 'limit'];
 
-  next(err);
+  // Percorre removeFields e deleta eles da query
+  removeFields.forEach((param) => delete reqQuery[param]);
+
+  let queryStr = JSON.stringify(reqQuery); // 1 - filtering is very simple with Express and Mongoose
+
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  ); // lte, gte, etc are mongoose operators, but it needs a money sign to work
+  // some URLs may be encoded prior to reaching your Node.js backend. In other words, the $ symbol could be represented as %24 in URLs.
+  // which is why it might not be best practice to have front-end send the $ in the request
+  console.log(queryStr);
+
+  // Traz recursos
+  query = Bootcamp.find(JSON.parse(queryStr)); // 2 - filtering is very simple with Express and Mongoose
+
+  // Seleciona campos a serem trazidos| Ex: {{URL}}/api/v1/bootcamps?select=name,description : Traz só os campos nome e descrição dos bootcamps
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  // Ordenação | Ex: {{URL}}/api/v1/bootcamps?sort=-name = Nome descendente: Z -> A
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    // Ordenação padrão caso não venha nada na query; Data de criação descendente
+    query = query.sort('-createdAt');
+  }
+
+  // Paginação || Número da página e número(limite) de recursos por página | Ex: {{URL}}/api/v1/bootcamps?limit=2&select=name
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
+  // Executa a query
+  const bootcamps = await query;
+
+  //Resultado da paginação
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit, // same as limit: limit
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    pagination,
+    data: bootcamps,
+  });
 });
 
 // @desc    Traz dados de um único bootcamp
@@ -73,4 +139,31 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   }
 
   res.status(200).json({ success: true, data: {} });
+});
+
+// @desc    Traz bootcamps dentro de um raio
+// @route   Post /api/v1/bootcamps/radius/:zipcode/:distance
+// @access  Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  // Traz longitude e latitude do geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lng = loc[0].longitude;
+  const lat = loc[0].latitude;
+
+  // Calc raio usando radianos
+  // Divide dist pelo raio da Terra
+  // Raio da Terra = 3963 milhas | 6378 quilômetros
+  const radius = distance / 3963;
+
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  });
 });
