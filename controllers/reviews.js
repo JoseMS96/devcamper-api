0c0449c7440dc502123e1eca4893adf0d15c0735
Jose <melo.jeduardo@gmail.com>
2024-08-18 18:03:50 -0300

    Reviews: Nota média, Update e Delete

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -41,3 +41,82 @@ exports.getReview = asyncHandler(async (req, res, next) => {
     data: review,
   });
 });
+
+// @desc    Cria uma review(avaliação) de um bootcamp
+// @route   POST /api/v1/bootcamps/:bootcampId/reviews
+// @access  Private
+exports.addReview = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId; // Pega o ID dos params da req e utiliza para selecionar o bootcamp que receberá a review || Link do bootcamp 1:N
+  req.body.user = req.user.id; // Utiliza o id do usuário logado para criar a review do bootcamp || Link do usuário 1:N
+
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(
+        `No bootcamp with the id of ${req.params.bootcampId}`,
+        404
+      )
+    );
+  }
+
+  const review = await Review.create(req.body);
+
+  res.status(200).json({
+    success: true,
+    data: review,
+  });
+});
+
+// @desc    Atualiza os dados review(avaliação) de um bootcamp
+// @route   PUT /api/v1/reviews/:id
+// @access  Private
+exports.updateReview = asyncHandler(async (req, res, next) => {
+  let review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No review with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Garante que a review(avaliação) pertence ao usuário
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`Not authorized to update review`, 401));
+  }
+
+  review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: review,
+  });
+});
+
+// @desc    Deleta uma review(avaliação) de um bootcamp
+// @route   DELETE /api/v1/reviews/:id
+// @access  Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No review with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Garante que a review(avaliação) pertence ao usuário
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`Not authorized to update review`, 401));
+  }
+
+  await review.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
--- models/Review.js
+++ models/Review.js
@@ -33,4 +33,41 @@ const ReviewSchema = new mongoose.Schema({
   },
 });
 
+// Previne que o usuário envie mais do que uma avaliação(review) por bootcamp
+ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
+
+// Método estático para calcular a nota média das reviews de um bootcamp
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      // Bootcamp é o documento(tabela em SQL) que recebe a avaliação(rating) média
+      averageRating: obj[0].averageRating, // Calculado acima
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Call getAverageCost after save
+ReviewSchema.post('save', function () {
+  this.constructor.getAverageRating(this.bootcamp);
+});
+
+// Call getAverageCost before remove
+ReviewSchema.post('deleteOne', { document: true, query: false }, function () {
+  this.constructor.getAverageRating(this.bootcamp);
+});
+
 module.exports = mongoose.model('Review', ReviewSchema);
